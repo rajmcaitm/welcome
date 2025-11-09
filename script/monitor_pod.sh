1aec253c1487e1dbce3107b8f25651745ff61a9d
Dharm <rajmca.itm@gmail.com>
2025-11-09 13:24:26 +0530

    csg-123_welcome_function: disabling liveness

--- script/monitor_pod.sh
+++ script/monitor_pod.sh
@@ -5,9 +5,9 @@ APP_LABEL="app=welcome"
 NOT_RUNNING=$(kubectl get pods -n "$NAMESPACE" -l "$APP_LABEL" --no-headers | grep -v Running | wc -l)
 
 if [ "$NOT_RUNNING" -eq 0 ]; then
-  echo "✅ All pods are running"
+  echo "All pods are running"
   exit 0
 else
-  echo "❌ Some pods are not running"
+  echo "Some pods are not running"
   exit 1
 fi
